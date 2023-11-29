d8098b5e3e176d9dab08b65bffbd850b7802b00a
yahyacodes <yahya.codin@gmail.com>
2023-11-29 13:37:21 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, contest):
+        self.contest = contest.lower()
+
+    def match(self, contest):
+        return [content for content in contest if self.anagram(content)]
+    
+    def anagram(self, content):
+        content_lower = content.lower()
+
+        if len(self.contest) != len(content_lower):
+            return False
+
+        return sorted(self.contest) == sorted(content_lower) 
